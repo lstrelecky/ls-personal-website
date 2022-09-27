0fa997fb846e655b58ec6e89d2f7e9639116b90d
lstrelecky2022 <lstrelecky2022@pingry.org>
2022-09-26 23:42:50 -0400

    Changed git config core.ignorecase false.

---
+++ components/profileheadline.js
@@ -0,0 +1,14 @@
+import { Heading, Text, Flex } from '@chakra-ui/react'
+import { Avatar, AvatarGroup } from "@chakra-ui/avatar"
+
+export default function ProfileHeadline() {
+  return (
+    <Flex direction="row" justify="space-between" align="center" mb={6}>
+        <Flex direction="column" justify="center" align="flex-start">
+            <Heading size="lg">Lukas Strelecky</Heading>
+            <Text>College Student - Webdev, Photography,  &#38; Medicine</Text>
+        </Flex>
+        <Avatar src="Headshot.JPG" size="xl" name="Lukas Strelecky"/>
+    </Flex>
+  )
+}
